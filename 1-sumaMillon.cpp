4aa90502517caeb9e7be3a52920a6cf7b9a3f19f
JulioYauri <83053778+JulioYauri@users.noreply.github.com>
2021-05-07 12:06:04 -0500

    Update 1-sumaMillon.cpp

--- 1-sumaMillon.cpp
+++ 1-sumaMillon.cpp
@@ -13,18 +13,25 @@ ll sumaIterativa(int* arr, int tam){
 	}
 	return suma;
 }
-
+//se utilizó goto para no consumir memoria de la pila
 ll sumaRecursiva(int* arr, int tam){
 	suma:
 		if(tam == 1) return *arr;
+		//se suma el contenido del ultimo elemento al penultimo
+		//hasta que se llegue al primer elemento
+		//el tamaño del arreglo va disminuyendo
 		*(arr + tam - 2) += *(arr + tam - 1);
 		tam--;
+		//llamada recursiva a la sentencia suma
 		goto suma;
 }
 
 int main(){
+	//num = 1000000
 	int tam = num;
 	int *arr = new int[num];
+	//generando numeros aleatorios para el arreglo
+	//todos los numeros son menores a 20
 	srand(time(NULL));
 	for(ll i = 0; i < num; i++){
 		*(arr + i) = rand() % 20;
