362bc007dbc07e9ad0718d02a94eecb59856ad14
JulioYauri <83053778+JulioYauri@users.noreply.github.com>
2021-05-07 12:13:52 -0500

    Update 4-cadenaTamano.cpp

--- 4-cadenaTamano.cpp
+++ 4-cadenaTamano.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int tamanoIterativa(string cadena){
+	//se inicia un contador en 0
+	//el contador aumenta hasta encontrar al carácter nulo
 	int tamano = 0;
 	for(int i = 0; cadena[i] != '\0'; i++, tamano++);
 	return tamano;
 }
 
 int tamanoRecursiva(string cadena){
+	//la recursion termina cuando la cadena es nula
 	if(cadena[0] == '\0')return 0;
 	else{
+		//se borra el ultimo elemento y se suma 1
 		cadena.pop_back();
 		return tamanoRecursiva(cadena) + 1;
 	}
