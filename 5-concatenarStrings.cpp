310a74516da4e5d5e3f1de7e688a82278bc9b29b
JulioYauri <83053778+JulioYauri@users.noreply.github.com>
2021-05-07 11:54:36 -0500

    Update 5-concatenarStrings.cpp

--- 5-concatenarStrings.cpp
+++ 5-concatenarStrings.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 void concatenar(char* a, char* b, int tamA, int tamB){
+	//se empieza a asignar cada char de la segunda cadena para concatenar
 	for(int i = 0; i < tamB; i++){
 		*(a + i + tamA) = *(b + i);	
 	}
+	//el ultimo char de la cadena final es el carácter nulo
 	*(a + tamA + tamB) = '\0';
 }
 
@@ -12,6 +14,7 @@ int main(){
 	string s1, s2;
 	cout << "Ingresa dos cadenas:\n" << flush;
 	cin >> s1 >> s2;
+	//asignando los dos punteros
 	char* p1 = &s1[0], *p2 = &s2[0];
 	int t1 = s1.size(), t2 = s2.size();
 	concatenar(p1, p2, t1, t2);
